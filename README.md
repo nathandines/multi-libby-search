f5bca836b2abef9ef9552fe5c55898b3b1a9b947
Nathan Dines <nathandines@users.noreply.github.com>
2023-07-27 09:29:49 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Multi Libby Search
+
+Libby doesn't search across multiple libraries. This is a simple page which will run a query over multiple libraries (opens in multiple tabs).
---
+++ app.js
@@ -0,0 +1,51 @@
+const searchInput = document.getElementById("search-input");
+const libraryKeyInput = document.getElementById("library-key-input");
+const searchButton = document.getElementById("search-button");
+const searchForm = document.getElementById("search-form");
+
+searchInput.addEventListener("input", setButtonState);
+libraryKeyInput.addEventListener("input", setButtonState);
+
+function setButtonState() {
+    searchButton.disabled = searchInput.value.length === 0 || libraryKeyInput.value.length === 0
+}
+
+function saveLibraries() {
+    let libArray = parseLibraries();
+    localStorage.setItem('libraries', JSON.stringify(libArray))
+}
+
+function loadLibraries() {
+    let storedVal = localStorage.getItem('libraries')
+    if (storedVal) {
+        libraryKeyInput.value = JSON.parse(storedVal).join('\n')
+    }
+}
+
+function parseLibraries() {
+    return libraryKeyInput.value.split('\n');
+}
+
+function generateLibbyUrl(libraryKey, query) {
+    let urlEncodedQuery = encodeURIComponent(query)
+    return "https://libbyapp.com/search/" + libraryKey + "/search/query-" + urlEncodedQuery + "/page-1"
+}
+
+function submit(event) {
+    event.preventDefault();
+    saveLibraries()
+
+    let searchQuery = searchInput.value
+    let libraryKeyArray = parseLibraries()
+    for (var i = 0; i < libraryKeyArray.length; i++) {
+        window.open(generateLibbyUrl(libraryKeyArray[i], searchQuery), '_blank')
+    }
+}
+
+function init() {
+    loadLibraries()
+    setButtonState()
+    searchForm.onsubmit = submit
+}
+
+window.onload = init;
